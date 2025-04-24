ad2d4d7db1d7a4c1615867afcfbb0d2d86e44b35
karthikkumar0498 <karthikkumargummulla0498@gmail.com>
2025-04-24 20:51:38 +0530

    Created a new branch and added a new class in it.

--- src/git_1/Class1_Of_Branch_1.java
+++ src/git_1/Class1_Of_Branch_1.java
@@ -5,7 +5,7 @@ public class Class1_Of_Branch_1
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Class1_Of_Branch_1");
+		System.out.println("Class1_Of_Branch_1 in new branch");
 	}
 
 }
